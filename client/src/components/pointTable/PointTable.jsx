7cc06441353e74a2471f9ec76df4c6a8434deb8c
midhunk0 <midhunrajk009@gmail.com>
2025-06-01 15:13:26 +0530

    made playoff map responsive and added it in admin page also

--- client/src/components/pointTable/PointTable.jsx
+++ client/src/components/pointTable/PointTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./PointTable.css";
 import { useSeason } from "../../context/seasonContext";
 import { useNavigate } from "react-router-dom";
--- client/src/pages/admin/home/Home.jsx
+++ client/src/pages/admin/home/Home.jsx
@@ -1,13 +1,15 @@
 // @ts-nocheck
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { useSeason, useYear } from "../../../context/seasonContext";
 import { PointTable } from "../../../components/pointTable/PointTable";
 import { toast } from "react-toastify";
 import { WinLoseTable } from "../../../components/winLoseTable/WinLoseTable";
 import { useTheme } from "../../../context/themeContext";
+import { useNavigate } from "react-router-dom";
 
 export function Home(){
+    const navigate=useNavigate();
     const apiUrl=import.meta.env.MODE==="development"
         ? import.meta.env.VITE_APP_DEV_URL 
         : import.meta.env.VITE_APP_PROD_URL
@@ -20,6 +22,19 @@ export function Home(){
     const [showForm, setShowForm]=useState(false);
     const [tableSwitch, setTableSwitch]=useState(true);
     const [hover, setHover]=useState("");
+    const [_, setWidth]=useState(window.innerWidth);
+    const [show, setShow]=useState(window.innerWidth>720);
+
+    useEffect(()=>{
+        function handleResize(){
+            const newWidth=window.innerWidth;
+            setWidth(newWidth);
+            setShow(newWidth>720);
+        }
+    
+        window.addEventListener("resize", handleResize);
+        return ()=>window.removeEventListener("resize", handleResize);
+    }, []);
 
     const { theme }=useTheme();
     const { year }=useYear();
@@ -27,6 +42,7 @@ export function Home(){
     if(!season){
         return;
     }
+    const playoffs=season.playoffs;
 
     function handleInputChange(e){
         setTeamData({
@@ -87,6 +103,14 @@ export function Home(){
         }
     }
 
+    function getFormatedDate(date){
+        return new Date(date).toLocaleDateString("en-IN", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        }).replace(/\//g, "-");   
+    }
+
     return(
         <div className="admin-home">
             <div className="admin-home-header">
@@ -108,6 +132,80 @@ export function Home(){
                     : 
                         <WinLoseTable teamDest="/admin/team"/>
                     }
+                    {playoffs && (
+                        <div className="admin-home-playoffs">
+                            <div className="admin-home-q1">
+                                <div className={`admin-home-playoff-team ${playoffs.qualifier1.result.won.short!=="" ? playoffs.qualifier1.result.won.short===playoffs.qualifier1.team.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.qualifier1.team.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.qualifier1.team.short}.png`} alt="q1-team" className="logo"/>
+                                    <h3>{playoffs.qualifier1.team.short || "TBD"}</h3>
+                                </div>
+                                <div className="admin-home-q1-details" onClick={()=>navigate(`/admin/matches/${season.playoffs.qualifier1._id}`)}>
+                                    <p>{show ? "qualifier 1" : "Q1"}</p>
+                                    <p>{getFormatedDate(playoffs.qualifier1.date)}, {playoffs.qualifier1.time}</p>
+                                    <p>{playoffs.qualifier1.venue}</p>
+                                </div>
+                                <div className={`admin-home-playoff-team ${playoffs.qualifier1.result.won.short!=="" ? playoffs.qualifier1.result.won.short===playoffs.qualifier1.opponent.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.qualifier1.opponent.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.qualifier1.opponent.short}.png`} alt="q1-opponent" className="logo"/>
+                                    <h3>{playoffs.qualifier1.opponent.short || "TBD"}</h3>
+                                </div>
+                            </div>
+                            <div className="admin-home-el">
+                                <div className={`admin-home-playoff-team ${playoffs.eliminator.result.won.short!=="" ? playoffs.eliminator.result.won.short===playoffs.eliminator.team.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.eliminator.team.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.eliminator.team.short}.png`} alt="el-team" className="logo"/>
+                                    <h3>{playoffs.eliminator.team.short || "TBD"}</h3>
+                                </div>
+                                <div className="admin-home-el-details" onClick={()=>navigate(`/admin/matches/${season.playoffs.eliminator._id}`)}>
+                                    <p>{show ? "eliminator" : "EL"}</p>
+                                    <p>{getFormatedDate(playoffs.eliminator.date)}, {playoffs.eliminator.time}</p>
+                                    <p>{playoffs.eliminator.venue}</p>
+                                </div>
+                                <div className={`admin-home-playoff-team ${playoffs.eliminator.result.won.short!=="" ? playoffs.eliminator.result.won.short===playoffs.eliminator.opponent.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.eliminator.opponent.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.eliminator.opponent.short}.png`} alt="el-opponent" className="logo"/>
+                                    <h3>{playoffs.eliminator.opponent.short || "TBD"}</h3>
+                                </div>
+                            </div>
+                            <div className="admin-home-q2">
+                                <div className={`admin-home-playoff-team ${playoffs.qualifier2.result.won.short!=="" ? playoffs.qualifier2.result.won.short===playoffs.qualifier2.team.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.qualifier2.team.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.qualifier2.team.short}.png`} alt="q2-team" className="logo"/>
+                                    <h3>{playoffs.qualifier2.team.short || "TBD"}</h3>
+                                </div>
+                                <div className="admin-home-q2-details" onClick={()=>navigate(`/admin/matches/${season.playoffs.qualifier2._id}`)}>
+                                    <p>{show ? "qualifier 2" : "Q2"}</p>
+                                    <p>{getFormatedDate(playoffs.qualifier2.date)}, {playoffs.qualifier2.time}</p>
+                                    <p>{playoffs.qualifier2.venue}</p>
+                                </div>
+                                <div className={`admin-home-playoff-team ${playoffs.qualifier2.result.won.short!=="" ? playoffs.qualifier2.result.won.short===playoffs.qualifier2.opponent.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.qualifier2.opponent.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.qualifier2.opponent.short}.png`} alt="q2-opponent" className="logo"/>
+                                    <h3>{playoffs.qualifier2.opponent.short || "TBD"}</h3>
+                                </div>
+                            </div>
+                            <div className="admin-home-final">
+                                <div className={`admin-home-playoff-team ${playoffs.final.result.won.short!=="" ? playoffs.final.result.won.short===playoffs.final.team.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.final.team.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.final.team.short}.png`} alt="final-team" className="logo"/>
+                                    <h3>{playoffs.final.team.short || "TBD"}</h3>
+                                </div>
+                                <div className="admin-home-final-details" onClick={()=>navigate(`/admin/matches/${season.playoffs.final._id}`)}>
+                                    <p>final</p>
+                                    <p>{getFormatedDate(playoffs.final.date)}, {playoffs.final.time}</p>
+                                    <p>{playoffs.final.venue}</p>
+                                </div>
+                                <div className={`admin-home-playoff-team ${playoffs.final.result.won.short!=="" ? playoffs.final.result.won.short===playoffs.final.opponent.short ? "won" : "lost" : ""}`}>
+                                    <img src={playoffs.final.opponent.short==="" ? theme==="light" ? "/logos/tbd-black.png" : "/logos/tbd-white.png" : `/logos/${playoffs.final.opponent.short}.png`} alt="final-opponent" className="logo"/>
+                                    <h3>{playoffs.final.opponent.short || "TBD"}</h3>
+                                </div>
+                            </div>
+                            <div className="playoff-line line-q1-final"></div>
+                            <div className="playoff-line line1-q1-to-q2"></div>
+                            <div className="playoff-line line2-q1-to-q2"></div>
+                            <div className="playoff-line line3-q1-to-q2"></div>
+                            <div className="playoff-line line1-q2-to-final"></div>
+                            <div className="playoff-line line2-q2-to-final"></div>
+                            <div className="playoff-line line3-q2-to-final"></div>
+                            <div className="playoff-line line1-el-to-q2"></div>
+                            <div className="playoff-line line2-el-to-q2"></div>
+                            <div className="playoff-line line3-el-to-q2"></div>
+                        </div>
+                    )}
                 </div>
             ) : (
                 <form className="admin-home-add-team-form" onSubmit={handleAddTeam} method="POST">
